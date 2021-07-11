b0598ee37641a7f67fa1ba04613a206b26626141
Nguyen Hoang Tam <nhtam1989@gmail.com>
2021-07-11 23:02:22 +0700

    document docker

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Document by Code Bushi`,
-    name: `Code Bushi`,
-    siteUrl: `https://gatsby-theme-document.netlify.com`,
-    description: `This is my description that will be used in the meta tags and important for search results`,
+    title: `Document by nhtam@gmail.com`,
     sidebarConfig: {
-      forcedNavOrder: ["/introduction", "/codeblock"],
+      forcedNavOrder: ["/introduction",
+        "/codeblock"
+      ],
       ignoreIndex: true
     }
   },
